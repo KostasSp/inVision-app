912ecad551774f2836718296590564a8d0c0b17d
Kostas <kostasspanolios@gmail.com>
2022-06-03 19:00:43 +0100

    update postAPI URI on frontend

--- client/src/components/dynamic-form/DynamicForm.jsx
+++ client/src/components/dynamic-form/DynamicForm.jsx
@@ -52,7 +52,7 @@ const DynamicForm = () => {
   //sends form data to an external API (made with a simple express.js backend)
   const sendToAPI = (e) => {
     e.preventDefault();
-    fetch("http://localhost:5000/getAPI", {
+    fetch("http://localhost:5000/postAPI", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formFields.entries()),
